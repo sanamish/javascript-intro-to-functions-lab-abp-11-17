e5aeb4d1be99c848a868fb5f2efd07433463161d
Kazi Sanam <sanamish@gmail.com>
2017-11-15 03:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string = 'hello') {
     return "I can't hear you!"
-  } else if (string.toUpperCase) {
+  } else if ("HELLO") {
     return "YES INDEED!"
-  } else if (string='I love you, Grandma.') {
+  } else if ('I love you, Grandma.') {
     return "I love you, too."
   }
   }
