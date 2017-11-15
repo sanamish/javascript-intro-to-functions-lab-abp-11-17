3ab6133d68549973b2a2f65768bb1c816fac6153
Kazi Sanam <sanamish@gmail.com>
2017-11-15 02:55:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase ()
+}
+
+
+function logShout(string) {
+  console.log.toUpperCase()
+
+function logWhisper(string) {
+  console.log.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+if (string.lowercase){
+  return "I can't hear you!"
+} else if (string.uppercase) {
+  return "YES INDEED!"
+} else if (string = 'I love you, Grandma.') {
+  return "I love you, too."
+}}
+
+  //var sayHiToGrandma = function
+  //sayHiToGrandma.toLowerCase() === lowercase
+    //return "I can`t hear you!"}
+  //else if (string.UpperCase) {
+    //return "YES INDEED!"}
+  //else if ('I love you, Grandma'){
+    //return "I love you, too."
+  //}}
