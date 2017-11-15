63141c3c07f81b395adf727140212814141cd440
Kazi Sanam <sanamish@gmail.com>
2017-11-15 03:35:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string="I love you, Grandma.") {
     return "I love you, too."
-} else if (string.toLowerCase) {
+} else if (string="I love you, too") {
     return "I can't hear you!"
-  } else if (string.toUpperCase) {
+  } else if (String="I can't hear you!") {
     return "YES INDEED!"
   }
   }
